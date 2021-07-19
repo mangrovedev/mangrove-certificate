e431b002fc85a53571bdac6e9ff2f745488c7f21
Plant a Mangrove <87638597+mangrovedev@users.noreply.github.com>
2021-07-19 20:46:34 +0530

    Update widget.js
    test auto resize iframe

--- widget.js
+++ widget.js
@@ -8,12 +8,12 @@
       iframe = document.createElement('iframe');
       iframe.setAttribute('src', widget_link.href);
       iframe.setAttribute('width', '100%');
-      //iframe.setAttribute('height', '600');
+      iframe.setAttribute('height', '600');
       iframe.setAttribute('frameborder', '0');
       iframe.setAttribute('scrolling', 'no');
       iframe.style.cssText="margin:0 auto";
       iframe.onload = function(){
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+        this.style.height = this.contentWindow.document.body.scrollHeight + 'px';
       }
       widget_link.parentNode.replaceChild(iframe, widget_link);
     }
